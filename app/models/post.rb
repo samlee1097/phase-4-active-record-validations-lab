bc61d34e60b2b42178f34268205d5924de65624f
Samuel <samlee1097@yahoo.com>
2021-11-02 17:54:15 -0400

    commit tree 8ad7eec27f52d12715f7a6a2bf6bec6c8802695b

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, :length => {:minimum => 250}
+    validates :summary, :length => {:maximum => 250}
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+    validate :is_clickbait
+
+      def is_clickbait
+        if title
+            unless title.match?(/Won't Believe|Secret|Guess/)
+                errors.add(:title, "Not Clickbait")
+            end
+        end
+    end
+
 end
